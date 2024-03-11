77f717083ce3c4e6545edc2b70b1b65db6785d75
Anthonys Gomez <anthonydavid.30@gmail.com>
2024-03-11 14:00:13 +0100

    Initial Commit

---
+++ KODI/KODIApp.swift
@@ -0,0 +1,32 @@
+//
+//  KODIApp.swift
+//  KODI
+//
+//  Created by Anthonys David Gomez Briceno on 11.03.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KODIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
